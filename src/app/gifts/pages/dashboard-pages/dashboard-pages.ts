749f491efba684cfbd15a707317c978de70e43fc
Joel Yovera <ingyovera6114@gmail.com>
2026-02-09 14:35:00 -0500

    feat(sidebar): sidebar using tailwind for styles

---
+++ src/app/gifts/pages/dashboard-pages/dashboard-pages.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-pages.html',
+})
+export default class DashboardPages { }
