9ab6cef8fc04c915343f44c649ad317d657f7cb7
Joel Yovera <ingyovera6114@gmail.com>
2026-02-09 14:35:25 -0500

    feat(routes): rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import DashboardPages from './gifts/pages/dashboard-pages/dashboard-pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        /**
+         * DIFERENCIAS ENTRE EL loadComponent: y component: 
+         * 
+         * 1.- component: carga este componente apenas abra la app
+         * 2.- loadComponent: no descargues este codigo hasta que el usuario haga click en la ruta
+         */
+        path:'dashboard',
+        loadComponent: () => import('./gifts/pages/dashboard-pages/dashboard-pages'),
+        children: [
+            {
+                path: 'trending',
+                loadComponent: () => import('./gifts/pages/trending/trending'),
+            }, 
+            {
+                path: 'search',
+                loadComponent: () => import('./gifts/pages/search/search'),
+            },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
