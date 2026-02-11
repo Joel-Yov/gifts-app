3e7c47b70d2ebfe249fdcee95ed4774b2f4f6141
Joel Yovera <ingyovera6114@gmail.com>
2026-02-11 12:15:32 -0500

    remover los archivos del enviroment del seguimiento

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-//AQUI VAN LAS VARIABLES DE ENTORNO DE PRODUCCION
-
-export const environment = {
-    production: true,
-    companyName: 'Gifts',
-    companyName2: 'App',
-    companySlogan: 'Maneja tus gifts'
-};
